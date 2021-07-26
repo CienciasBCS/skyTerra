13885b61b54a7f337396ed6fb6e7bb27ef016d87
René Fernando <renefernandoortega@gmail.com>
2021-07-26 13:18:18 -0600

    config prod

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql://{os.environ['DB_USER_SB']}:{os.environ['DB_PASS_SB']}@{os.environ['DB_HOST_SB']}/{os.environ['DB_NAME_PROD']}"
+        f"mysql://{os.environ['DB_USER_SB']}:{os.environ['DB_PASS_SB']}@{os.environ['DB_HOST_SB']}/{os.environ['DB_NAME']}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DB_SB_URI = f"mysql://{os.environ['DB_USER_SB']}:{os.environ['DB_PASS_SB']}@{os.environ['DB_HOST_SB']}/{os.environ['DB_NAME_SB_TAR']}"
