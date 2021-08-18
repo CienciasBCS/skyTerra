d66935c025c05b1787d1f237d6ceba8f743edc61
René Fernando <renefernandoortega@gmail.com>
2021-08-18 10:30:18 -0600

    admin blueprint fix bug

--- app/solarbeam/admin/__init__.py
+++ app/solarbeam/admin/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('solarbeam_app', __name__, url_prefix='/solarbeam/app/admin')
+bp = Blueprint('solarbeam_admin', __name__, url_prefix='/solarbeam/app/admin')
 
 from . import routes
